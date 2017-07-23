19ade1d087513ebd67da3da20d9d2f118ce50a50
Steven Wang <3460883@gmail.com>
2017-07-23 16:39:57 -0700

    Refactor tot.el
    - attach tot prefix to all functions
    - break down text copying step

--- tot.el
+++ tot.el
@@ -3,21 +3,27 @@
 
 (defun tot-write-here ()
   (interactive)
-  (setq tot-write-buffer-path (buffer-file-name))
-  (setq tot-write-buffer-name (buffer-name)))
+  (tot-set-write-buffer-vars))
 
 (defun tot-read-here ()
   (interactive)
-  (add-hook 'after-change-functions #'text-copy nil t))
+  (add-hook 'after-change-functions #'tot-process-text nil t))
 
 (defvar tot-write-buffer-path nil)
 (defvar tot-write-buffer-name nil)
 
-(defun text-copy (beg end _len)
+(defun tot-set-write-buffer-vars ()
+  (setq tot-write-buffer-path (buffer-file-name))
+  (setq tot-write-buffer-name (buffer-name)))
+
+(defun tot-process-text (beg end _len)
   (let ((text (buffer-substring-no-properties beg end)))
-    (write-region text
-                  nil
-                  tot-write-buffer-path
-                  'append)
-    (set-buffer tot-write-buffer-name)
-    (revert-buffer t t)))
+    (tot-paste-text text)))
+
+(defun tot-paste-text (text)
+  (write-region text
+                nil
+                tot-write-buffer-path
+                'append)
+  (set-buffer tot-write-buffer-name)
+  (revert-buffer t t))
